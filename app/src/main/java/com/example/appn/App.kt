6b5f6b32207d6e9b7b88d0356b90672adf67a914
sufulu <sufulu@siwei.com>
2024-03-10 22:51:13 +0800

    init

--- app/src/main/java/com/example/appn/App.kt
+++
@@ -1,9 +0,0 @@
-package com.example.appn
-
-import com.appn.core_common.base.BaseApplication
-
-class App : BaseApplication() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
--- core/core-common/src/main/java/com/appn/core_common/mvvm/BaseViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.appn.core_common.mvvm
-
-import androidx.lifecycle.ViewModel
-
-class BaseViewModel : ViewModel() {
-}
